7040b0d914a9506ba143c72739faccb65ab6517b
David Peer <david.peer@outlook.com>
2024-08-14 16:10:44 +0200

    Execute all benchmarks again

--- scripts/run_die.sh
+++ scripts/run_die.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# ./scripts/run.sh kleister_charity $1
-# ./scripts/run.sh kleister_nda $1
-# ./scripts/run.sh sroie $1
+./scripts/run.sh kleister_charity $1
+./scripts/run.sh kleister_nda $1
+./scripts/run.sh sroie $1
 ./scripts/run.sh vrdu_ad_buy $1
-# ./scripts/run.sh vrdu_registration $1
+./scripts/run.sh vrdu_registration $1
